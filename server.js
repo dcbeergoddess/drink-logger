ee999909bf594f68b9305eb3674313f03c3a56eb
Rachel Murray <dcbeergoddess@gmail.com>
2020-05-14 20:55:37 -0400

    creating file structure and adding boilerplate code

---
+++ server.js
@@ -0,0 +1,48 @@
+// Dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql")
+const config = require("./config/db")
+
+// Create an instance of the express app.
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//by default use local settings
+let dbConfig = config.local
+//change if in production
+if( process.env.NODE_ENV === "production") {
+  dbConfig = config.production
+}
+
+const connection = mysql.createConnection(dbConfig);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// ROUTES
+    //DO WE WANT TO PUT THESE IN A SEPERATE CONTROLLER FOLDER
+
+
+//LISTEN    
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
